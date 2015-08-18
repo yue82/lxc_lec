c296896acd68fb206e1887325a5593ab22652a4f
yue82 <no.82.vuf.lab@gmail.com>
2015-08-18 18:33:18 +0900

    Fix by review

---
+++ deploy-lxc_review.py
@@ -0,0 +1,50 @@
+#! /usr/bin/python
+# encode:utf-8
+import subprocess
+import sys
+
+def main():
+
+    lxcList=["ubuntu-nginx"]
+    lxcSocatList=["ubuntu-ap1", "ubuntu-ap2"]
+    
+    # Destroy same name LXC
+    for lxcName in lxcList+lxcSocatList:
+        lxcNameSet = ["-n", lxcName]
+        cmdStop = ["/usr/bin/lxc-stop"] + lxcNameSet
+        cmdDestroy = ["/usr/bin/lxc-destroy"] + lxcNameSet
+        
+        # 失敗してもよしとする
+        subprocess.call(cmdStop)
+        subprocess.call(cmdDestroy)
+
+    try:
+        # Create and Start
+        for lxcName in lxcList+lxcSocatList:
+            lxcNameSet = ["-n", lxcName]
+            cmdCreate = ["/usr/bin/lxc-create", "-t", "ubuntu"] + lxcNameSet
+            cmdStart = ["/usr/bin/lxc-start"] + lxcNameSet + ["-d"]
+          
+            subprocess.check_call(cmdCreate)
+            subprocess.check_call(cmdStart)
+
+        # deploy Socat
+        for lxcName in lxcSocatList:
+            lxcNameSet = ["-n", lxcName]
+            cmdAttach = ["/usr/bin/lxc-attach"] + lxcNameSet + ["--"]
+            onSh = ["/bin/sh", "-c"] 
+            
+            attachInstallSocat = ["/usr/bin/apt-get", "install", "-y", "socat"]
+            socatExec = "/bin/hostname"
+            attachSocat = ["/usr/bin/socat TCP4-LISTEN:8000,fork,reuseaddr EXEC:\""+socatExec+"\" &"]
+          
+            subprocess.check_call(cmdAttach + attachInstallSocat)
+            subprocess.check_call(cmdAttach + onSh + attachSocat)
+            
+    except subprocess.CallProcessError, (p):
+        print "Failure"
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
+
