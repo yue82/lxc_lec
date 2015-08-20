62eaff465ff0b40cde4a9645d2f14cca7d50cfaa
yue82 <no.82.vuf.lab@gmail.com>
2015-08-20 10:24:10 +0900

    remove try-except add lxclist pattern

--- aptget-lxc.py
+++ aptget-lxc.py
@@ -6,28 +6,25 @@ import sys
 
 def main():
 
-    lxcList=["ubuntu-ap1", "ubuntu-ap2", "ubuntu-nginx"]
+    # lxcList=["ubuntu-ap1", "ubuntu-ap2", "ubuntu-nginx"]
+    # lxcList=["ubuntu-ap1", "ubuntu-ap2"]
+    lxcList=["ubuntu-nginx"]
     
     parser = argparse.ArgumentParser()
     parser.add_argument('pp', metavar='PP', help='print lines containing this pattern.')
     args = parser.parse_args()
     insTool = args.pp
 
-    try:
-        # deploy Socat
-        for lxcName in lxcList:
-            lxcNameSet = ["-n", lxcName]
-            cmdAttach = ["/usr/bin/lxc-attach"] + lxcNameSet + ["--"]
-            
-            attachInstall = ["/usr/bin/apt-get", "install", "-y", insTool]
-          
-            subprocess.check_call(cmdAttach + attachInstall)
-            print "#"*10 + "end install on " + lxcName + "#"*10
-
-    except subprocess.CallProcessError, (p):
-        print 'subprocess.CalledProcessError: cmd:%s returncode:%s' % (p.cmd, p.returncode)
-        sys.exit(1)
+    # deploy Socat
+    for lxcName in lxcList:
+        lxcNameSet = ["-n", lxcName]
+        cmdAttach = ["/usr/bin/lxc-attach"] + lxcNameSet + ["--"]
         
+        attachInstall = ["/usr/bin/apt-get", "install", "-y", insTool]
+        
+        subprocess.check_call(cmdAttach + attachInstall)
+        print "#"*10 + "end on " + lxcName + "#"*10
+            
 if __name__ == '__main__':
     main()
 
