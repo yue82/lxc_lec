9d3473b246f7ec4286517edba44fbd54fa0db132
yue82 <no.82.vuf.lab@gmail.com>
2015-08-20 14:31:33 +0900

    get ip addr of aps

--- deploy-nginx.py
+++ deploy-nginx.py
@@ -1,27 +1,32 @@
 #! /usr/bin/python3
 # encode:utf-8
+import subprocess
 
 def main():
     lxcApList = ["ubuntu-ap1", "ubuntu-ap2"]
     lxcNginxList = ["ubuntu-nginx"]
-    apIpAddr = {}
     
     username = "ubuntu"
     lxcPath = "/var/lib/lxc/"
     lxcRoot = "/rootfs"
 
-    # get Aps ip address
-    for lxcName in lxcApList:
-        apIpAddr[lxcName] = 00
+    service = "nginx"
 
-    
+    # get Aps ip address    
+    apIpAddr = {}
+    cmdGetLxcInfo = ["/usr/bin/lxc-ls", "--fancy"]
+    copInfo = subprocess.check_output(cmdGetLxcInfo).decode("utf-8")
+    for info in copInfo.strip().split("\n")[2:]:
+        infolist = info.strip().split()
+        apIpAddr[infolist[0]] = infolist[2]
+
     for lxcName in lxcNginxList:
         # nginx install
         insNginx = ["./aptget-lxc.py", "nginx", lxcName]
-        subprocess.check_call(insNginx)
+        subprocess.check_output(insNginx)
 
         # stop nginx service 
-
+        cmdStopService = ["/bin/service", service, "stop"]
 
         # make location config
 
@@ -30,7 +35,8 @@ def main():
         # write configs
 
         # start nginx service
-
-
+        cmdStartService = ["/bin/service", service, "start"]
+        
+        
 if __name__ == '__main__':
     main()
